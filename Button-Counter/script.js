817f9129559c2bef1b62157c2de1378213d771d6
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2026-04-07 14:25:53 +0000

    opa

---
+++ Button-Counter/script.js
@@ -0,0 +1,7 @@
+const btn-soma = document.querySelector('btn-soma')
+const btn-subtracao = document.querySelector('btn-subtracao')
+const result = document.querySelector('#result')
+
+btnsoma.addEventListener("click", function(){
+    
+})
